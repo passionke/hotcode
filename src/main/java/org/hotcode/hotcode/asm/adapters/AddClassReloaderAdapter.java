55ea35b2db32a5ebe3a411c777a55b3a9420d3bc
unknown <yong.zhuy@ali-24504n.hz.ali.com>
2013-07-05 09:16:37 +0800

    test

--- src/main/java/org/hotcode/hotcode/asm/adapters/AddClassReloaderAdapter.java
+++ src/main/java/org/hotcode/hotcode/asm/adapters/AddClassReloaderAdapter.java
@@ -1,13 +1,10 @@
 package org.hotcode.hotcode.asm.adapters;
 
-import java.lang.reflect.Modifier;
-
+import org.hotcode.hotcode.ClassReloader;
+import org.hotcode.hotcode.constants.HotCodeConstant;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
-
-import org.hotcode.hotcode.ClassReloader;
-import org.hotcode.hotcode.constants.HotCodeConstant;
 
 /**
  * Add {@link org.hotcode.hotcode.ClassReloader} to the class.
@@ -22,10 +19,10 @@ public class AddClassReloaderAdapter extends ClassVisitor {
 
     @Override
     public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-        if (!Modifier.isInterface(access)) {
-            cv.visitField(Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC, HotCodeConstant.HOTCODE_CLASS_RELOADER_FIELDS,
-                          Type.getDescriptor(ClassReloader.class), null, null);
-        }
+        // if (!Modifier.isInterface(access)) {
+        cv.visitField(Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC, HotCodeConstant.HOTCODE_CLASS_RELOADER_FIELDS,
+                      Type.getDescriptor(ClassReloader.class), null, null);
+        // }
         super.visit(version, access, name, signature, superName, interfaces);
     }
 }
